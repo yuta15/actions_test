42512fe8d115df91c9a9ec5c71cdbba2db00faa6
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-07-06 18:12:02 +0900

    test

--- run_python.py
+++ run_python.py
@@ -1,12 +1,19 @@
+import json
 import sys
 import os
 import argparse
 
 
 def main(args: str):
-    sys.stdout.write(f'{args}testのmain関数')
-
-
+    data = {
+        "config": {
+            "test": args
+        }
+    }
+    os.mkdir("test")
+    with open("test/config.json", "w") as f:
+        json.dump(data, f, indent=4)
+    sys.stdout.write(os.path.dirname(os.path.abspath("test/config.json")))
 
 
 if __name__ == '__main__':
