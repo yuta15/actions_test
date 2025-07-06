dc4240bf1e046dc0c3973e8dd091b7acd4f7977d
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-07-06 19:03:14 +0900

    Azure APIを使用するように修正

--- fetch_rgs.py
+++ fetch_rgs.py
@@ -8,6 +8,7 @@ from azure.mgmt.resource import ResourceManagementClient
 
 def fetch_rgs():
     SUBSCRIPTION_ID = os.environ.get("AZURE_SUBSCRIPTION_ID")
+    print(SUBSCRIPTION_ID)
     credentail = DefaultAzureCredential()
     client = ResourceManagementClient(credential=credentail, subscription_id=SUBSCRIPTION_ID)
     resource_groups = client.resource_groups.list()
