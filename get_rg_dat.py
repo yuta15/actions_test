468f988f7be0eda660d5e7214f40bb2d5767885d
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2025-07-06 19:49:45 +0900

    test

--- get_rg_dat.py
+++ get_rg_dat.py
@@ -8,16 +8,17 @@ from azure.identity import DefaultAzureCredential
 
 
 def main(rg_ids: list):
-    credential = DefaultAzureCredential()
-    subscription_id = os.environ.get("AZURE_SUBSCRIPTION_ID")
-    client = ResourceManagementClient(credential, subscription_id)
-    rg_ids = json.loads(rg_ids)
-    rgs = []
-    for id in rg_ids:
-        name = id.split('/')[-1]
-        rg = client.resource_groups.get(resource_group_name=name)
-        rgs.append(rg.as_dict())
-    json.dump(rgs, sys.stdout, indent=4)
+    sys.stdout.write(rg_ids)
+    # credential = DefaultAzureCredential()
+    # subscription_id = os.environ.get("AZURE_SUBSCRIPTION_ID")
+    # client = ResourceManagementClient(credential, subscription_id)
+    # rg_ids = json.loads(rg_ids)
+    # rgs = []
+    # for id in rg_ids:
+    #     name = id.split('/')[-1]
+    #     rg = client.resource_groups.get(resource_group_name=name)
+    #     rgs.append(rg.as_dict())
+    # json.dump(rgs, sys.stdout, indent=4)
 
 
 if __name__ == '__main__':
